84f391a71dbb554ca7f2f8d2eb770b2ef38a77e0
phaydara <phaydara@gmail.com>
2022-06-26 13:20:06 -0500

    renders hello world with data component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script>
   export default {
     // component options
-    
+
+    // data component option is a function that renders an object
+    data() {
+      return {
+        message: 'Hellow World!'
+      }
+    }
+
   }
 </script>
 
 <template>
-<!-- hard coded -->
-  <h1>Hello World!</h1>
+  <!-- hard coded -->
+  <!-- <h1>Hello World!</h1> -->
+  
+  <!-- how to render dynamic text based on value of `message` -->
+  <h1>{{ message }}</h1>
 </template>
 
